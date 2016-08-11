df603c3257685abb10eb454923be45081bf2dde3
Антишин Антон < >
2016-08-11 20:16:14 +0300

    snake

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -25,7 +25,8 @@ namespace Snake
             //отрисовка точек
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
         }
     }
 }
